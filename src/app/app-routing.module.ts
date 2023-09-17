f2fe6c9615bd93ecbf00e81e7d9ced6dea4e2a3c
Yuri Santana <yuri.luana2016@gmail.com>
2023-09-17 15:09:32 -0300

    signin and signup pages structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { SiginComponent } from './components/pages/sigin/sigin.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigupComponent } from './components/pages/sigup/sigup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: SiginComponent,
+  },
+  {
+    path: 'signup',
+    component: SigupComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
