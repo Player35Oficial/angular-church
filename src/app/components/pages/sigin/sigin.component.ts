ddac757a54da44e548c02a88e1a3cc1943fab787
Yuri Santana <yuri.luana2016@gmail.com>
2023-09-20 17:25:16 -0300

    add sign and sign form component initial structure

--- src/app/components/pages/sigin/sigin.component.ts
+++ src/app/components/pages/sigin/sigin.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sigin',
   templateUrl: './sigin.component.html',
-  styleUrls: ['./sigin.component.css']
+  styleUrls: ['./sigin.component.css'],
 })
 export class SiginComponent {
+  loginForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email')!;
+  }
+
+  get password() {
+    return this.loginForm.get('password')!;
+  }
+
+  submit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+    console.log('enviou formulário');
+  }
 }
--- src/app/components/pages/sigup/sigup.component.ts
+++ src/app/components/pages/sigup/sigup.component.ts
@@ -1,10 +1,45 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sigup',
   templateUrl: './sigup.component.html',
-  styleUrls: ['./sigup.component.css']
+  styleUrls: ['./sigup.component.css'],
 })
 export class SigupComponent {
+  newUserForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.newUserForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      role: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get username() {
+    return this.newUserForm.get('username')!;
+  }
+
+  get email() {
+    return this.newUserForm.get('email')!;
+  }
+
+  get password() {
+    return this.newUserForm.get('password')!;
+  }
+
+  get role() {
+    return this.newUserForm.get('role')!;
+  }
+
+  submit() {
+    if (this.newUserForm.invalid) {
+      console.log(this.newUserForm.errors);
+      return;
+    }
+    console.log(this.newUserForm.value);
+    console.log('formulário enviado!');
+  }
 }
